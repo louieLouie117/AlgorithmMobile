c53d25383b12b1d0c1fbc1e98880a58acbdb564a
Luis Cardona <cardona-luis@outlook.com>
2021-04-28 18:40:43 -0700

    needleInHayStack

---
+++ 2021/April/april.js
@@ -0,0 +1,18 @@
+// Wednesday April 28----------------------------------- 
+// find the needle in the hayStack
+function findNeedle(hayStack) {
+    console.log(hayStack)
+    for(i = 0; i < hayStack.length; i++ ){
+      console.log(hayStack[i])
+      
+      if(hayStack[i] === "needle"){
+        console.log("found the the needle at postion", i)
+        return "found the needle at position " + i;
+      }
+      
+    }
+  
+  // your code here
+}
+
+test
